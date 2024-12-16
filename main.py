c918bbfac850254fff1533e49a12559d7b4c1e82
coza_nostra7 <begzodmuminovb@gmail.com>
2024-12-16 06:44:50 +0500

    Day 21

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ with col1:
     st.image("images/my_photo.png")
     content = """
     Hi, Im Bekhzod. Im a python developer!
+    Ambitious and knowledge-thirsty person, team player and
+    game-changer in this rapidly developing world.
+
     """
     st.write(content)
+
+content2= """
+Below you can find app developed by me in Python. Feel free to send me a message!
+"""
+st.write(content2)
