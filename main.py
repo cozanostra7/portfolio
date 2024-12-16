f7c9be7ae51f226c54abd8d37d2ca67832a6cf49
coza_nostra7 <begzodmuminovb@gmail.com>
2024-12-16 06:41:26 +0500

    create columns,image and descrpt

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
+st.set_page_config(layout='wide')
 
 col1,col2 = st.columns(2)
 
 with col1:
     st.image("images/my_photo.png")
+    content = """
+    Hi, Im Bekhzod. Im a python developer!
+    """
+    st.write(content)
