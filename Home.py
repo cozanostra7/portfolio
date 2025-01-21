6cb9789223cc73cd80b22d67212e6aec1b3a46d5
coza_nostra7 <begzodmuminovb@gmail.com>
2025-01-21 22:24:04 +0500

    change

--- Home.py
+++ Home.py
@@ -23,7 +23,7 @@ st.write(content2)
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pd.read_csv('data.csv',sep=";")
 with col3:
-    for index, row in df[10:].iterrows():
+    for index, row in df[0:].iterrows():
         st.header(row['title'])
         st.write(row['description'])
         st.image("images/" + row['image'])
